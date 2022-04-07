9e3b69a7c8f8f352a2c38a5b365aa2bda68471ce
vikramsingh13 <vikramsingh.webdev@gmail.com>
2022-04-07 02:24:29 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+  state = {error: ""};
+
+  render(){
+    return(
+      <div>
+        <h1>React Pomodoro Clock</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
