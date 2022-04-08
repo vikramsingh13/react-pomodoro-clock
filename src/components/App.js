8921bd4a0f82a735ca9388a9d7c719a4b21377c5
vikramsingh13 <vikramsingh.webdev@gmail.com>
2022-04-08 09:21:17 -0400

    all the timer buttons working, git add .setInterval timer going too fast, needs fix git add .some restructuring of the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,84 @@
 import React, {Component} from 'react';
-import Timer from './Timer';
 import EditTimer from './EditTimer';
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      minutes: 25,
-      seconds: 0,
+      minutes: "01",
+      seconds: "15",
       start: false,
       edit: false,
+      interval: setInterval(this.countdown, 1000)
     };
   }
 
+  componentDidMount(){
+  }
+
   //helper functions
-  editMinutes = (e) => {
-    console.log(e.target.value);
-    this.setState({minutes: e.target.value});
-    console.log(this.state.minutes);
+  editMinutes = (newMinutes) => {
+    newMinutes = newMinutes < 10 ? ("0" + newMinutes) : newMinutes;
+    this.setState({minutes: newMinutes});
   }
 
-  editSeconds = (e) => {
-    this.setState({seconds: e.target.value});
+  editSeconds = (newSeconds) => {
+    newSeconds = newSeconds < 10 ? ("0" + newSeconds) : newSeconds;
+    this.setState({seconds: newSeconds});
   }
 
-  //changes value for this.start
-  pressedStart = () => {
-    this.setState({start: true, edit: false});
-    console.log(this.state.start);
+  //buttons for the timer
+  pressedStart = async() => {
+    if(!this.state.start){
+      this.setState({
+        start: true, 
+        edit: false,
+        interval: await setInterval(this.countdown, 1000),
+      });
+    }
   }
 
-  pressedPause = () => {
+  pressedPause = async() => {
     this.setState({start: false});
+    await clearInterval(this.state.interval);
+    this.setState({interval: null});
   }
 
-  pressedEdit = () => {
+  pressedEdit = async() => {
     this.setState({start: false, edit: true});
+    await clearInterval(this.state.interval);
+    this.setState({interval: null});
   }
 
-  pressedReset = () => {
-    this.setState({minutes: 25, seconds: 0, start: false, edit: false});
+  pressedReset = async() => {
+    this.setState({minutes: "25", seconds: "00", start: false, edit: false});
+    await clearInterval(this.state.interval);
+    this.setState({interval: null})
   }
+
+  countdown = () => {
+    if(this.state.start){
+      //when seconds reach 0, seconds is set to 59 and minutes is reduced by 1
+      if(parseInt(this.state.seconds) === 0){
+        this.editMinutes(parseInt(this.state.minutes) - 1);
+        this.editSeconds(59);
+      } else {
+        this.editSeconds(parseInt(this.state.seconds) - 1);
+      }
+    }
+  }
+
 
   render(){
     return(
       <div className="app">
         <h1>React Pomodoro Clock</h1>
 
-        <Timer minutes={this.state.minutes}
-        seconds={this.state.seconds} 
-        start={this.state.start}/>
+        <div className="timer">
+          <h2>{this.state.minutes} </h2> 
+          <h2>:</h2> 
+          <h2>{this.state.seconds} </h2>
+        </div>
 
         <EditTimer
         edit={this.state.edit} 
--- src/components/EditTimer.js
+++ src/components/EditTimer.js
@@ -3,46 +3,29 @@ import './EditTimer.css';
 
 class EditTimer extends Component{
 
-	constructor(props){
-		super(props);
-		this.state = {
-			minutes: props.minutes,
-			seconds: props.seconds,
-			edit: props.edit,
-			editStatus: "no-edit",
-		};
-	}
+	editStatus = this.props.edit ? "edit" : "no-edit";
 
-	componentDidMount(){
-		this.setState({minutes: this.props.minutes, seconds: this.props.seconds, editStatus: this.state.edit ? "edit" : "no-edit"})
+	componentDidUpdate(){
+		this.editStatus = this.props.edit ? "edit" : "no-edit";
 	}
 
 	//helper functions
 	newMinutes(e){
-		this.setState({minutes: parseInt(e.target.value)});
 		this.props.editMinutes(e);
 	}
 
 	newSeconds(e){
-		this.setState({seconds: parseInt(e.target.value)});
 		this.props.editSeconds(e);
 	}
 
 	render(){
 		return(
-			<div className={`edit-timer ${this.state.editStatus}`}>
-				<input type="text" value={this.state.minutes} onChange={(e) => {this.newMinutes(e)}}/>
-				<input type="text" value={this.state.seconds} onChange={(e) => {this.newSeconds(e)}}/>
+			<div className={`edit-timer ${this.editStatus}`}>
+				<input type="text" value={this.props.minutes} onChange={(e) => {this.newMinutes(e.target.value)}}/>
+				<input type="text" value={this.props.seconds} onChange={(e) => {this.newSeconds(e.target.value)}}/>
 			</div>
 		);
 	}
 }
-
-EditTimer.defaultProps = {
-	minutes: 0,
-	seconds: 0,
-	edit: false,
-	editStatus: "no-edit",
-};
 
 export default EditTimer;
